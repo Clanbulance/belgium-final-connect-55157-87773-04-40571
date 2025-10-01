a6b5333ba7d4812b5a66c21ce97e14812a1b0960
Clanbulance <107610779+Clanbulance@users.noreply.github.com>
2025-10-01 16:14:28 +0200

    changed vite.config base

--- vite.config.ts
+++ vite.config.ts
@@ -10,7 +10,7 @@ export default defineConfig(({ mode }) => ({
     port: 8080,
   },
   plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
-  base: '/belgium-final-connect-55157-87773-04-40571/',
+  base: '/',
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
